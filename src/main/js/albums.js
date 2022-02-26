3a683bcbf7940695a6559e13a5299a873486ad61
Levente Katai-Pal <levente.katai-pal@student.kdg.be>
2022-02-26 16:48:07 +0100

    Adding new album with ajax, updating table

--- src/main/js/albums.js
+++ src/main/js/albums.js
@@ -27,11 +27,21 @@ async function deleteAlbum(event) {
 }
 
 // Create
-const addNewAlbumButton = document.getElementById("add-album");
+const newAlbumForm = document.getElementById("new-album-form");
+
+// newAlbumForm.onsubmit = function () {
+//     console.log("Submitting a new album");
+//     return false;
+// }
+
+newAlbumForm.onsubmit = submitNewAlbum
+
+document.getElementById("add-album").onclick = showNewAlbumModal
+
 const newAlbumModal = document.getElementById("new-album-modal");
 const cancelNewAlbumModal = document.getElementsByClassName("q-cancel-modal")[0];
+const albumsTable = document.getElementById("albums-table");
 
-addNewAlbumButton.onclick = showNewAlbumModal
 cancelNewAlbumModal.onclick = hideNewAlbumModal
 
 function showNewAlbumModal() {
@@ -43,43 +53,66 @@ function hideNewAlbumModal() {
 }
 
 // Todo: urgent: NetworkError???
-function submitNewAlbum() {
-    console.log("Submitting new album");
-
+ function submitNewAlbum() {
     const fd = new FormData(document.getElementById("new-album-form"));
 
-    console.log("Keys: ");
+    console.log("About to submit new album");
+    console.log("FormData keys:");
     for(let key of fd.keys()) {
         console.log(key);
     }
 
-    console.log("Values: ");
+    console.log("FormData values: ");
     for(let key of fd.values()) {
         console.log(key)
     }
+    console.log("Submitting new album");
 
-    try {
-        fetch('/api/albums', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "title": fd.get("title"),
-                "release": fd.get("release"),
-                "description": fd.get("description"),
-                "songIds": fd.getAll("song-ids")
-            })
-        }).then(
-            response => {
-                console.log(response);
-            }
-        )
-    } catch (e) {
-        console.error(e);
-    }
+    // Function must return false to prevent page refresh after the onsubmit event
 
-    hideNewAlbumModal();
+     fetch('/api/albums', {
+         method: "POST",
+         headers: {
+             "Content-Type": "application/json"
+         },
+         body: JSON.stringify({
+             "title": fd.get("title"),
+             "release": fd.get("release"),
+             "description": fd.get("description"),
+             "songIds": fd.getAll("song-ids")
+         })
+     }).then(response => {
+         if (response.status === 200) {
+             response.json().then(data => {
+                 let rows = albumsTable.rows;
+                 let newRow = rows[rows.length - 1].cloneNode(true);
+                 let actionButtons = newRow.getElementsByTagName("button");
+
+                 newRow.id = `album-${data.id}`;
+                 newRow.children[0].innerText = data.id;
+                 newRow.children[1].innerText = data.title;
+                 newRow.children[2].innerText = data.release;
+                 newRow.children[3].innerText = data.songIds.length;
+
+                 actionButtons[0].value = data.id;
+                 actionButtons[0].onclick = showReadAlbumModal
+                 actionButtons[1].value = data.id;
+                 actionButtons[1].onclick = showEditAlbumModal;
+                 actionButtons[2].value = data.id;
+                 actionButtons[2].onclick = deleteAlbum
+
+                 albumsTable.append(newRow);
+
+                 console.log(newRow);
+             })
+
+         } else {
+             console.log(response.status);
+         }
+     })
+
+     hideNewAlbumModal();
+     return false; // To prevent onsubmit triggering a page refresh
 }
 
 // Update
